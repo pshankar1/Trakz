16875bbe680dbe1191b1b325ee285cf35d6be575
user name <65860449+pshankar1@users.noreply.github.com>
2022-04-19 00:37:54 -0400

    more widget stuff.. firestore return attempt

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_signin/screens/signin_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import '../widgets/widget.dart';
 //import 'package:spotify_sdk/spotify_sdk.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +12,10 @@ class HomeScreen extends StatefulWidget {
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
+}
+
+void onSearch() async {
+  await FirebaseAuth.instance.fetchSignInMethodsForEmail("identifiers");
 }
 
 class _HomeScreenState extends State<HomeScreen> {
@@ -40,37 +47,80 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: SafeArea(
-        child: Card(
-          elevation: 10,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(24),
-          ),
-          color: Color.fromARGB(113, 102, 86, 1),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                FlutterLogo(size: 48),
-                SizedBox(
-                  height: 16,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView.builder(
+            itemCount: 2,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.all(16),
+                child: Stack(
                   children: <Widget>[
-                    Text(
-                      'Title of the Card',
-                      style: Theme.of(context).textTheme.headline5,
+                    Card(
+                      elevation: 12,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      color: Color.fromARGB(255, 168, 163, 120),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 24.0, vertical: 36),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          gradient: LinearGradient(colors: [
+                            Color.fromARGB(0, 228, 171, 86),
+                            Color.fromARGB(255, 58, 52, 2)
+                          ]),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            FlutterLogo(size: 48),
+                            SizedBox(
+                              width: 16,
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text('Title of the Card',
+                                      style:
+                                          Theme.of(context).textTheme.headline5
+                                      //.copyWith(color: Colors.white),
+                                      ),
+                                  ElevatedButton(
+                                      child: Text('hey'),
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all(
+                                                Colors.red),
+                                        //  .copyWith(color: Colors.white)),
+                                      ))
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    Text('Some description',
-                        style: Theme.of(context).textTheme.headline6),
+                    Positioned(
+                      right: 6,
+                      bottom: 5,
+                      child: ElevatedButton(
+                          child: Text('hey'),
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.red),
+                            //  .copyWith(color: Colors.white)),
+                          )),
+                    ),
                   ],
                 ),
-              ],
-            ),
-          ),
-        ),
+              );
+            }),
       ),
+      //RowDemo(),
     );
   }
 }
