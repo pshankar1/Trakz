c2f35a29edf19a37f0d91129ed196902f8b6762d
user name <65860449+pshankar1@users.noreply.github.com>
2022-04-18 23:10:31 -0400

    first widget blank demo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,8 +15,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("app")),
+      backgroundColor: Color.fromARGB(111, 66, 95, 66),
+      appBar: AppBar(
+        title: Text("app"),
+        backgroundColor: Colors.black,
+      ),
+      // ignore: prefer_const_constructors
       bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
         child: ElevatedButton(
           // color: const Color.fromARGB(255, 90, 105, 91),
           child: const Text("Logout"),
@@ -33,7 +39,38 @@ class _HomeScreenState extends State<HomeScreen> {
           },
         ),
       ),
-      body: Center(),
+      body: SafeArea(
+        child: Card(
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          color: Color.fromARGB(113, 102, 86, 1),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                FlutterLogo(size: 48),
+                SizedBox(
+                  height: 16,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Title of the Card',
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    Text('Some description',
+                        style: Theme.of(context).textTheme.headline6),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
